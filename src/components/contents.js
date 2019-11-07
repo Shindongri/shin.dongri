fb4631253a82654e391a27954005c0e5465a1445
shindongri <tlsehdfl@naver.com>
2019-11-07 13:48:40 +0900

    .

--- src/components/contents.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Card from "./card"
-
-const Contents = ({ contents }) => (
-  contents.map(({ title, subtitle, created }) => <Card title={ title } subtitle={ subtitle } created={ created } />)
-)
-
-export default Contents
