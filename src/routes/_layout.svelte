ae9ed1cd098e02691cd65b1a89534de92f4d351b
shindongri <tlsehdfl@naver.com>
2021-06-20 23:27:07 +0900

    add fastcampus-210620.md

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -46,7 +46,7 @@
 
   <footer>
     <span>
-      &copy; {new Date().getFullYear()} Dong-ri 🍷.
+      &copy; {new Date().getFullYear()} Dong-ri.
       Powered by <a href="https://sapper.svelte.dev" target="_blank">Sapper</a>.
     </span>
   </footer>
